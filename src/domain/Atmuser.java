f2c742798d86e6d3bfe8b40156aec9827534739d
dragon <2072707750@qq.com>
2021-12-26 23:59:00 +0800

    sixth commit

--- src/domain/Atmuser.java
+++ src/domain/Atmuser.java
@@ -5,5 +5,11 @@ public class Atmuser {
     private String name;
     private String password;
 
+    public Atmuser() {
+    }
 
+    public Atmuser(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
 }
